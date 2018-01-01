8449ec4fd8b8059c1e1fd2d1a0a2da572b458ca3
daniel <daniel@bogusemailserver.com>
2018-01-01 14:08:58 +0000

    README

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # What's this?
 
-A package containing additional examples for
+A Haskel package containing additional examples for
 [lens-aeson](http://hackage.haskell.org/package/lens-aeson). It might be
-helpful for people who are not very familiar with
+helpful for people who are not yet very familiar with
 [lens](https://hackage.haskell.org/package/lens).
 
+# How to use this package?
+
+Read the haddock documentation.
+
+You can also play with the examples in the *repl*, by downloading the [Git
+repo](https://github.com/danidiaz/lens-aeson-examples), building it, and
+invoking
+
+    $ cabal repl lens-aeson-examples
+    Preprocessing library for lens-aeson-examples-1.0.0.0..
+    GHCi, version 8.2.2: http://www.haskell.org/ghc/  :? for help
+    Loaded GHCi configuration from /root/.ghci
+    [1 of 1] Compiling Data.Aeson.Lens.Examples ( src/Data/Aeson/Lens/Examples.hs, interpreted )
+    Ok, one module loaded.
+    *Data.Aeson.Lens.Examples> pp Null -- we have Data.Aeson, Data.Aeson.Lens and Control.Lens in scope
+    null
 
 
--- src/Data/Aeson/Lens/Examples.hs
+++ src/Data/Aeson/Lens/Examples.hs
@@ -1,1 +1,40 @@
-module Data.Aeson.Lens.Examples where
+-- |
+-- Main module description goes here.
+-- 
+
+module Data.Aeson.Lens.Examples (
+      -- * Pretty printing
+      pp
+      -- * Re-exports
+      -- $reexports
+    , module Data.Aeson
+    , module Data.Aeson.Lens
+    , module Control.Lens
+    ) where
+
+import qualified Data.ByteString.Lazy.Char8
+import           Data.Text
+import           Data.Aeson
+import           Data.Aeson.Encode.Pretty
+import           Data.Aeson.Lens
+import           Control.Lens hiding ((.=)) -- conflict with a Data.Aeson operator.
+
+-- http://hackage.haskell.org/package/haddock
+-- http://haskell-haddock.readthedocs.io/en/latest/index.html
+
+{-|
+    Pretty print a 'Value' to @stdout@.
+-}
+pp :: Value -> IO ()
+pp = Data.ByteString.Lazy.Char8.putStrLn . encodePretty
+
+{- $reexports
+
+    The following modules are re-exported for convenience:
+
+    - "Data.Aeson".
+    - "Data.Aeson.Lens".
+    - The whole of "Control.Lens" except the ('Control.Lens.Setter..=')
+      operator which conflicts with ('Data.Aeson.Types..=') from "Data.Aeson".
+ 
+-}
