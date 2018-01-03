9e914c609890b8debaf4891214aa1bf8591e3f02
daniel <daniel@bogusemailserver.com>
2018-01-03 22:13:45 +0000

    new examples

--- src/Data/Aeson/Lens/Examples.hs
+++ src/Data/Aeson/Lens/Examples.hs
@@ -22,6 +22,7 @@ module Data.Aeson.Lens.Examples (
     ) where
 
 import qualified Data.ByteString.Lazy.Char8
+import           Data.Monoid
 import           Data.Text
 import           Data.Map
 import           Data.Aeson
@@ -29,10 +30,6 @@ import           Data.Aeson.Encode.Pretty
 import           Data.Aeson.Lens
 import           Control.Lens hiding ((.=)) -- conflict with a Data.Aeson operator.
 import           GHC.Generics
-
--- http://hackage.haskell.org/package/haddock
--- http://haskell-haddock.readthedocs.io/en/latest/index.html
--- https://github.com/sol/doctest#readme
 
 data Pet = Dog 
          | Cat 
@@ -54,48 +51,47 @@ data Person = Person
             } deriving (Show,Eq,Generic)    
 instance ToJSON Person
 
-{- $setup
->>> :set -XOverloadedStrings
->>> import Data.Monoid
--}
+-- | $setup
+-- >>> :set -XOverloadedStrings
+--
 
-{-| 
-
->>> pp persons  
-[
-    {
-        "age": 43,
-        "name": "Alice",
-        "pets": {
-            "Fido": "Dog",
-            "Luna": "Cat"
-        },
-        "hobbies": [
-            "Running",
-            "Reading"
-        ]
-    },
-    {
-        "age": 50,
-        "name": "Bob",
-        "pets": {},
-        "hobbies": [
-            "Surfing",
-            "Cooking",
-            "Reading"
-        ]
-    },
-    {
-        "age": 51,
-        "name": "Jim",
-        "pets": {
-            "Pluto": "Dog"
-        },
-        "hobbies": []
-    }
-]
-
--}
+-- | 
+-- 
+-- >>> pp persons  
+-- [
+--     {
+--         "age": 43,
+--         "name": "Alice",
+--         "pets": {
+--             "Fido": "Dog",
+--             "Luna": "Cat"
+--         },
+--         "hobbies": [
+--             "Running",
+--             "Reading"
+--         ]
+--     },
+--     {
+--         "age": 50,
+--         "name": "Bob",
+--         "pets": {},
+--         "hobbies": [
+--             "Surfing",
+--             "Cooking",
+--             "Reading"
+--         ]
+--     },
+--     {
+--         "age": 51,
+--         "name": "Jim",
+--         "pets": {
+--             "Pluto": "Dog"
+--         },
+--         "hobbies": []
+--     }
+-- ]
+-- 
+-- 
 persons :: Value
 persons = 
     toJSON $
@@ -113,125 +109,131 @@ persons =
              (Data.Map.fromList [("Pluto",Dog)])
     ]
 
-{- $searching
+-- | $searching
+-- 
+-- == Getting the names of all persons
+-- 
+-- >>> persons^..values.key "name"._String
+-- ["Alice","Bob","Jim"]
+-- 
+-- == Getting the name of the second person
+-- 
+-- >>> persons^..nth 1.key "name"._String
+-- ["Bob"]
+-- 
+-- == Getting the names of all persons not named Bob.
+-- 
+-- We use 'filtered' in together with 'hasn't' and 'only'.
+-- 
+-- 'has' and 'hasn't' are useful combinators that check if a 'Fold' hits or does
+-- not hit any targets.
+-- 
+-- 'only' is a strange 'Prism' that only matches in the case of equality,
+-- returning an uninformative @()@. But this is enough to use it with 'has' and
+-- 'hasn't'.
+-- 
+-- >>> persons^..values.key "name"._String.filtered (hasn't (only "Bob"))
+--["Alice","Jim"]
+-- 
+-- We can also use simple functions as arguments to 'filtered':
+-- 
+-- >>> persons^..values.key "name"._String.filtered (\name -> name /= "Bob")
+--["Alice","Jim"]
+-- 
+-- The nice thing about 'filtered' is that it doen't yank us out of the lensy
+-- world; we can keep composing with 'Fold's or 'Traversal's. Here we compose with
+-- 'each', a traversal which lets us visit the elemets of monomorphic containers
+-- like 'Text':
+-- 
+-- >>> persons^..values.key "name"._String.filtered (hasn't (only "Bob")).each
+-- "AliceJim"
+-- 
+-- == Getting the ages of all persons not named Bob.
+-- 
+-- Here the situation is a bit different: we are filtering depending on a value
+-- (the person's name) that we do not want to extract.
+-- 
+-- >>> persons^..values.filtered (hasn't (key "name"._String.only "Bob")).key "age"._Integer
+-- [43,51]
+-- 
+-- Notice that we now filter on the person objects /directly/, and move the search
+-- for the name /inside/ the filtering condition. Then we extract the ages from the
+-- filtered person objects.
+-- 
+-- >>> :{
+--     let noBob person = case preview (key "name"._String) person of
+--             Just name | name == "Bob" -> False
+--             _ -> True
+--     in persons^..values.filtered noBob.key "age"._Integer
+--     :}
+--[43,51]
+-- 
+-- >>> persons^..values.filtered (noneOf (key "name"._String) (=="Bob")).key "age"._Integer
+--[43,51]
+-- 
+-- == Getting the names of all persons who like reading or cooking
+-- 
+-- 'Fold's can be pasted togeter using ('Data.Monoid.<>'), which is sometimes useful:
+-- 
+-- >>> persons^..values.filtered (has (key "hobbies".values._String.(only "Reading"<>only "Cooking"))).key "name"._String
+--["Alice","Bob"]
+-- 
+-- Another way of saying the same, using the 'anyOf' combinator:
+-- 
+-- >>> persons^..values.filtered (anyOf (key "hobbies".values._String) (\t -> t=="Reading" || t=="Cooking")).key "name"._String
+--["Alice","Bob"]
+-- 
+-- == Getting the names of all persons whose every pet is a dog 
+-- 
+-- >>> persons^..values.filtered (allOf (key "pets".members._String) (=="Dog")).key "name"._String
+--["Bob","Jim"]
+-- 
+-- == Getting the names of all pets
+-- 
+-- Notice that the pet names are /keys/ in an object.
+-- 
+-- 'members' is an 'IndexedTraversal', and we can compose it with 'asIndex' to
+-- extract the object keys:
+-- 
+-- >>> persons^..values.key "pets".members.asIndex
+--["Fido","Luna","Pluto"]
+-- 
+-- == Getting the types of pets not named Luna
+-- 
+-- Filtering based on object keys is done using the 'indices' function.
+-- 
+-- >>> persons^..values.key "pets".members.indices (/="Luna")._String
+-- ["Dog","Dog"]
+-- 
+-- 'values' is also an 'IndexedTraversal'; the index is the position on the
+-- array. We can filter based on that:
+-- 
+-- >>> persons^..values.indices even.key "name"._String
+-- ["Alice","Jim"]
+-- 
+-- == Matching each hobby with the index of the person in the person array
+-- >>> itoListOf (((values<.key "hobbies")<.values)<._String) persons
+--[(0,"Running"),(0,"Reading"),(1,"Surfing"),(1,"Cooking"),(1,"Reading")]
+--
+-- == Matching each pet type with the owner's index and the pet's name
+-- >>> itoListOf (((values<.key "pets")<.>members)<._String) persons
+--[((0,"Fido"),"Dog"),((0,"Luna"),"Cat"),((2,"Pluto"),"Dog")]
+--
 
-== Getting the names of all persons
-
->>> persons^..values.key "name"._String
-["Alice","Bob","Jim"]
-
-== Getting the name of the second person
-
->>> persons^..nth 1.key "name"._String
-["Bob"]
-
-== Getting the names of all persons not named Bob.
-
-We use 'filtered' in together with 'hasn't' and 'only'.
-
-'has' and 'hasn't' are useful combinators that check if a 'Fold' hits or does
-not hit any targets.
-
-'only' is a strange 'Prism' that only matches in the case of equality,
-returning an uninformative @()@. But this is enough to use it with 'has' and
-'hasn't'.
-
->>> persons^..values.key "name"._String.filtered (hasn't (only "Bob"))
-["Alice","Jim"]
-
-We can also use simple functions as arguments to 'filtered':
-
->>> persons^..values.key "name"._String.filtered (\name -> name /= "Bob")
-["Alice","Jim"]
-
-The nice thing about 'filtered' is that it doen't yank us out of the lensy
-world; we can keep composing with 'Fold's or 'Traversal's. Here we compose with
-'each', a traversal which lets us visit the elemets of monomorphic containers
-like 'Text':
-
->>> persons^..values.key "name"._String.filtered (hasn't (only "Bob")).each
-"AliceJim"
-
-== Getting the ages of all persons not named Bob.
-
-Here the situation is a bit different: we are filtering depending on a value
-(the person's name) that we do not want to extract.
-
->>> persons^..values.filtered (hasn't (key "name"._String.only "Bob")).key "age"._Integer
-[43,51]
-
-Notice that we now filter on the person objects /directly/, and move the search
-for the name /inside/ the filtering condition. Then we extract the ages from the
-filtered person objects.
-
->>> :{
-    let noBob person = case preview (key "name"._String) person of
-            Just name | name == "Bob" -> False
-            _ -> True
-    in persons^..values.filtered noBob.key "age"._Integer
-    :}
-[43,51]
-
->>> persons^..values.filtered (noneOf (key "name"._String) (=="Bob")).key "age"._Integer
-[43,51]
-
-== Getting the names of all persons who like reading or cooking
-
-'Fold's can be pasted togeter using ('Data.Monoid.<>'), which is sometimes useful:
-
->>> persons^..values.filtered (has (key "hobbies".values._String.(only "Reading"<>only "Cooking"))).key "name"._String
-["Alice","Bob"]
-
-Another way of saying the same, using the 'anyOf' combinator:
-
->>> persons^..values.filtered (anyOf (key "hobbies".values._String) (\t -> t=="Reading" || t=="Cooking")).key "name"._String
-["Alice","Bob"]
-
-== Getting the names of all persons whose every pet is a dog 
-
->>> persons^..values.filtered (allOf (key "pets".members._String) (=="Dog")).key "name"._String
-["Bob","Jim"]
-
-== Getting the names of all pets
-
-Notice that the pet names are /keys/ in an object.
-
-'members' is an 'IndexedTraversal', and we can compose it with 'asIndex' to
-extract the object keys:
-
->>> persons^..values.key "pets".members.asIndex
-["Fido","Luna","Pluto"]
-
-== Getting the types of pets not named Luna
-
-Filtering based on object keys is done using the 'indices' function.
-
->>> persons^..values.key "pets".members.indices (/="Luna")._String
-["Dog","Dog"]
-
-'values' is also an 'IndexedTraversal'; the index is the position on the
-array. We can filter based on that:
-
->>> persons^..values.indices even.key "name"._String
-["Alice","Jim"]
-
--}
-
-{-|
-    Pretty print a 'Value' to @stdout@.
--}
+-- |
+-- Pretty print a 'Value' to @stdout@.
+--
 pp :: Value -> IO ()
 pp = Data.ByteString.Lazy.Char8.putStrLn . encodePretty
 
-{- $reexports
-
-    The following modules are re-exported for convenience:
-
-    - "Data.Aeson".
-    - "Data.Aeson.Lens".
-    - The whole of "Control.Lens" except the ('Control.Lens.Setter..=')
-      operator which conflicts with ('Data.Aeson.Types..=') from "Data.Aeson".
- 
--}
+-- | $reexports
+--
+-- The following modules are re-exported for convenience:
+--
+-- - "Data.Aeson".
+-- - "Data.Aeson.Lens".
+-- - The whole of "Control.Lens" except the ('Control.Lens.Setter..=')
+--   operator which conflicts with ('Data.Aeson.Types..=') from "Data.Aeson".
+-- 
 
 
